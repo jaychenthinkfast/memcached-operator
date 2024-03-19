72b4fb371ae76e4b021beb3b090caf77f527b22e
Jay Chen <chenjie@chenjie.info>
2024-03-19 10:24:08 +0800

    Reconcile role gcr.io linuxamd64 sampleyaml

--- internal/controller/memcached_controller.go
+++ internal/controller/memcached_controller.go
@@ -44,7 +44,7 @@ type MemcachedReconciler struct {
 //+kubebuilder:rbac:groups=cache.chenjie.info,resources=memcacheds/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=cache.chenjie.info,resources=memcacheds/finalizers,verbs=update
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
